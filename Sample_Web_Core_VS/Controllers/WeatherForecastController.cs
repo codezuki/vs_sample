8f1f056a66cfe003859706d4edaf4624941cf9d1
Faizur Rahman <faizur@angainfotech.in>
2025-04-23 10:28:54 +0530

    new api added-weatherforcast

--- Sample_Web_Core_VS/Controllers/WeatherForecastController.cs
+++ Sample_Web_Core_VS/Controllers/WeatherForecastController.cs
@@ -29,5 +29,16 @@ namespace Sample_Web_Core_VS.Controllers
             })
             .ToArray();
         }
+        [HttpGet(Name = "GetWeatherForecast_new")]
+        public IEnumerable<WeatherForecast> GetNew()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
