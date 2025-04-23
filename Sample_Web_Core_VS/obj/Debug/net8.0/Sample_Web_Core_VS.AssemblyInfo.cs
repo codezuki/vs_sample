bd6f40229f755f646024608040cbf00fe3eb57b3
Faizur Rahman <faizur@angainfotech.in>
2025-04-23 12:57:21 +0530

    comment

--- Sample_Web_Core_VS/obj/Debug/net8.0/Sample_Web_Core_VS.AssemblyInfo.cs
+++ Sample_Web_Core_VS/obj/Debug/net8.0/Sample_Web_Core_VS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sample_Web_Core_VS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f2395a3e7843fe191af23a9162a5e328e1339a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50a2f20ce91bfacc17ad73d0e5e41b876b92c1b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sample_Web_Core_VS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sample_Web_Core_VS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
